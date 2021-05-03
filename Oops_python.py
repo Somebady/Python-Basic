863c3ff9e709e426622ba0ad6975a5292442e2fa
Somebady <akshaysoni460@gmail.com>
2021-05-03 14:42:53 +0530

    commit

--- Oops_python.py
+++ Oops_python.py
@@ -1,153 +1,229 @@
-# OOPS in Python
-class Car():
+# # OOPS in Python
+# class Car():
 
-    # def __new__(self, doors, windows, engine):
-    #     print('Class Object is called')
+#     # def __new__(self, doors, windows, engine):
+#     #     print('Class Object is called')
 
-    def __init__(self, doors, windows, engine):
-        self.__doors__ = doors
-        self.windows = windows
-        self.engine = engine
-        print('Object Intialization is completed')
+#     def __init__(self, doors, windows, engine):
+#         self.__doors__ = doors
+#         self.windows = windows
+#         self.engine = engine
+#         print('Object Intialization is completed')
 
-    def get_details(self):
-        print(self.__doors__, self.windows, self.engine)
-
-
-car1 = Car(4, 5, 'Electric')
-print(type(car1))
-car1.get_details()
-print(car1.engine)
-print(car1.__doors__)
+#     def get_details(self):
+#         print(self.__doors__, self.windows, self.engine)
 
 
-# Magic Methods in classes
-print(dir(car1))  # All default __fn__ is called magic or dunker fn..
-print(car1.__sizeof__())
+# car1 = Car(4, 5, 'Electric')
+# print(type(car1))
+# car1.get_details()
+# print(car1.engine)
+# print(car1.__doors__)
 
 
-# Class Methods and Class Variables
-class Bank_account():
-    roi = '10%'  # Class variable
-
-    def __init__(self, name, age, account_number):
-        self.name = name
-        self.age = age
-        self.account_number = account_number
-
-    @classmethod  # Class Method
-    def revised_roi(cls, roi):
-        cls.roi = roi
+# # Magic Methods in classes
+# print(dir(car1))  # All default __fn__ is called magic or dunker fn..
+# print(car1.__sizeof__())
 
 
-person1 = Bank_account('Akshay Soni', 24, 99261)
+# # Class Methods and Class Variables
+# class Bank_account():
+#     roi = '10%'  # Class variable
 
-print(person1.name, person1.account_number)
-print(person1.roi)
+#     def __init__(self, name, age, account_number):
+#         self.name = name
+#         self.age = age
+#         self.account_number = account_number
 
-Bank_account.revised_roi('12%')
-print(person1.roi)
+#     @classmethod  # Class Method
+#     def revised_roi(cls, roi):
+#         cls.roi = roi
 
-print(Bank_account.roi)
 
-person1.revised_roi('13%')
-print(Bank_account.roi)
+# person1 = Bank_account('Akshay Soni', 24, 99261)
 
-# person1.roi = '20%'
+# print(person1.name, person1.account_number)
 # print(person1.roi)
 
-print(Bank_account.roi)
+# Bank_account.revised_roi('12%')
+# print(person1.roi)
 
-Bank_account.roi = '30%'
-print(Bank_account.roi)
+# print(Bank_account.roi)
 
-print(person1.roi)
+# person1.revised_roi('13%')
+# print(Bank_account.roi)
 
-person3 = Bank_account('Akshay', 24, '9999')
-print(person3.roi)
+# # person1.roi = '20%'
+# # print(person1.roi)
+
+# print(Bank_account.roi)
+
+# Bank_account.roi = '30%'
+# print(Bank_account.roi)
+
+# print(person1.roi)
+
+# person3 = Bank_account('Akshay', 24, '9999')
+# print(person3.roi)
 
 
-# Class variable exist even when the object is not created &
-# it is a class variable so it is same for all objects.
+# # Class variable exist even when the object is not created &
+# # it is a class variable so it is same for all objects.
 
-# Class Method is called by both class & object of class, but it is standard or Genuine way to call
-# Class method only with class name
+# # Class Method is called by both class & object of class, but it is standard or Genuine way to call
+# # Class method only with class name
 
 
-# Access Specifier in python
-class Bank_account1():
-    roi = '10%'  # Class variable
+# # Access Specifier in python
+# class Bank_account1():
+#     roi = '10%'  # Class variable
 
-    def __init__(self, name, age, account_number, pin):
-        self.name = name  # By default it is public variable
+#     def __init__(self, name, age, account_number, pin):
+#         self.name = name  # By default it is public variable
+#         self.age = age
+
+#         # making Protected Variable
+#         self._account_number = account_number  # Protected Variable
+
+#         # making Private Varaible
+#         self.__pin = pin
+
+#     @classmethod  # Class Method
+#     def revised_roi(cls, roi):
+#         cls.roi = roi
+
+
+# person4 = Bank_account1('Akshay Soni', 22, 9090, 123)
+# print(dir(person4))
+# print(person4._account_number)
+# print(person4._Bank_account1__pin)
+
+# person4._Bank_account1__pin = 456
+# person4._account_number = 8080
+# print(person4._account_number)
+# print(person4._Bank_account1__pin)
+
+# # Public -- It is accessible from all object of class
+# # Protected -- it should only be accessible from object of inherited class only
+# # Private -- it should never be directly accessible outside the class
+
+# # Imp--In Python, Access specifier is not strongly mapped as in Java & C++, Here everything is accessible, even private variable
+# # But as a sensible developer access specifier is indicate developer what is the accessibility of that variable
+# # and he should follow this, but there is no restriction is present here, All of this is just a indication
+
+
+# # Exception Handling
+# a = 10
+# b = 10
+# try:
+#     x = a+b
+#     y = a/b
+#     z = 22
+# except ZeroDivisionError as e:
+#     print('Can not divide by Zero')
+# except NameError as e:
+#     print('Variable is not define', e)
+# except Exception as e:
+#     print('Exception', e)
+# else:
+#     # Else block will execute only when there was no exception genrate in the try block
+#     print(x)
+#     print(y)
+#     print(z)
+# finally:
+#     print('Execution is done')
+#     # Finally will execute always, either code raise exception or not
+#     # Practice use of finally is when you open connection to database, but later on code raise exception so in finally block you will be able to
+#     # to close the database connection
+
+
+# # Custom Exceptions
+# # Defining Custom Exception
+# class MyException(Exception):
+#     pass
+
+
+# try:
+#     age = int(input())
+#     if age > 20:
+#         print('valid')
+#     else:
+#         raise MyException  # raise my custom exception
+# except MyException as e:
+#     print('Not Eligible to Apply')
+
+# try:
+#     age = int(input())
+#     if age > 20:
+#         print('valid')
+#     else:
+#         raise Exception
+# except Exception as e:
+#     print('Not Eligble to Apply', e)
+
+# try:
+#     age = int(input())
+#     assert age > 20
+#     print('valid')
+# except AssertionError as e:
+#     print('Not Eligble to Apply')
+
+
+# Inheritance
+
+class Emp():
+    def __init__(self, name, age):
+        self.name = name
         self.age = age
 
-        # making Protected Variable
-        self._account_number = account_number  # Protected Variable
 
-        # making Private Varaible
-        self.__pin = pin
-
-    @classmethod  # Class Method
-    def revised_roi(cls, roi):
-        cls.roi = roi
+class Emp_Acc_info(Emp):
+    def __init__(self, name, age, salary, account):
+        super().__init__(name, age)  # Super is used to call fn of parent class
+        self.salary = salary
+        self.account = account
 
 
-person4 = Bank_account1('Akshay Soni', 22, 9090, 123)
-print(dir(person4))
-print(person4._account_number)
-print(person4._Bank_account1__pin)
-
-person4._Bank_account1__pin = 456
-person4._account_number = 8080
-print(person4._account_number)
-print(person4._Bank_account1__pin)
-
-# Public -- It is accessible from all object of class
-# Protected -- it should only be accessible from object of inherited class only
-# Private -- it should never be directly accessible outside the class
-
-# Imp--In Python, Access specifier is not strongly mapped as in Java & C++, Here everything is accessible, even private variable
-# But as a sensible developer access specifier is indicate developer what is the accessibility of that variable
-# and he should follow this, but there is no restriction is present here, All of this is just a indication
+e1 = Emp('Akshay', 24)
+print(e1.name, e1.age)
+e2 = Emp_Acc_info('Akshay Soni', 27, 6000, 12837)
+print(e2.name, e2.age, e2.salary, e2.account)
 
 
-# Exception Handling
-a = 10
-b = 10
-try:
-    x = a+b
-    y = a/b
-    z = 22
-except ZeroDivisionError as e:
-    print('Can not divide by Zero')
-except NameError as e:
-    print('Variable is not define', e)
-except Exception as e:
-    print('Exception', e)
-else:
-    # Else block will execute only when there was no exception genrate in the try block
-    print(x)
-    print(y)
-    print(z)
-finally:
-    print('Execution is done')
-    # Finally will execute always, either code raise exception or not
-    # Practice use of finally is when you open connection to database, but later on code raise exception so in finally block you will be able to
-    # to close the database connection
+class A():
+    def __init__(self):
+        pass
+
+    def assign(self, name):
+        print(f'A:{name}')
 
 
-# Custom Exceptions
-# Defining Custom Exception
-class MyException(Exception):
-    pass
+class B(A):
+    def __init__(self):
+        pass
+
+    def assign(self, name):
+        print(f'B:{name}')
 
 
-try:
-    age = int(input())
-    if age > 20:
-        print('valid')
-    else:
-        raise MyException  # raise my custom exception
-except MyException as e:
-    print('Not Eligible to Apply')
+class C(A):
+    def __init__(self):
+        pass
+
+    def assign(self, name):
+        print(f'C:{name}')
+
+
+class D(B, C):
+    def __init__(self):
+        pass
+
+    def assign(self, name):
+        print(f'D:{name}')
+        super().assign(name)
+        C.assign(self, name)
+        A.assign(self, name)
+
+
+d1 = D()
+d1.assign('Akshay')
